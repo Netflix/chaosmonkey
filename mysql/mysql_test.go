6c61774cbcf0fdbf761e96376366dfcfc5f91793
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-06-18 19:39:15 -0400

    spelling: database
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- mysql/mysql_test.go
+++ mysql/mysql_test.go
@@ -155,7 +155,7 @@ func startMySQLContainer() (*exec.Cmd, error) {
 }
 
 // initDB initializes the "chaosmonkey" database with the chaosmonkey schemas
-// It wipes out any existing database database with the same name
+// It wipes out any existing database with the same name
 func initDB() error {
 	db, err := sql.Open("mysql", fmt.Sprintf("root:%s@tcp(127.0.0.1:%d)/", password, port))
 	if err != nil {
